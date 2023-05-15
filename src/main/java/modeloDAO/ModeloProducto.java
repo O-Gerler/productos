f55aadbcee653368bd9f1c9651e67933d4ef7262
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-15 13:40:23 +0200

    getAllProductos

--- src/main/java/modeloDAO/ModeloProducto.java
+++ src/main/java/modeloDAO/ModeloProducto.java
@@ -1,5 +1,39 @@
 package modeloDAO;
 
-public class ModeloProducto {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import modeloDTO.Producto;
+
+public class ModeloProducto extends Conector{
+	public ArrayList<Producto> getAllProductos() {
+		String st = "select * from productos";
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		try {
+			PreparedStatement pst = super.con.prepareStatement(st);
+			
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				Producto producto = new Producto();
+				
+				producto.setId(rs.getInt("id"));
+				producto.setCodigo(rs.getString("codigo"));
+				producto.setNombre(rs.getString("nombre"));
+				producto.setCantidad(rs.getInt("cantidad"));
+				producto.setPrecio(rs.getDouble("precio"));
+				producto.setCaducidad(rs.getDate("caducidad"));
+				
+				productos.add(producto);
+			}
+			
+			return productos;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
