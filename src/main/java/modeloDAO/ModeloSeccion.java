dd15cd8a31ab0818bd70b9ca076cbdd161223ecc
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-15 13:54:55 +0200

    get seccion por id

--- src/main/java/modeloDAO/ModeloSeccion.java
+++ src/main/java/modeloDAO/ModeloSeccion.java
@@ -1,5 +1,34 @@
 package modeloDAO;
 
-public class ModeloSeccion {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import modeloDTO.Seccion;
+
+public class ModeloSeccion extends Conector{
+	public Seccion getSeccion(int id) {
+		String st = "select * from secciones where id=?";
+		
+		try {
+			PreparedStatement pst = super.con.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			ResultSet rs = pst.executeQuery();
+			rs.next();
+			
+			Seccion seccion = new Seccion();
+			
+			seccion.setId(rs.getInt("id"));
+			seccion.setNombre(rs.getString("nombre"));
+			
+			return seccion;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
