c7f114dbefeba0f2f64d429dceb403be16cdc48c
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-24 14:08:08 +0200

    clase

---
+++ src/main/java/modeloDTO/SuperProducto.java
@@ -0,0 +1,19 @@
+package modeloDTO;
+
+public class SuperProducto {
+	private Producto producto;
+	private Supermercado supermercado;
+
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public Supermercado getSupermercado() {
+		return supermercado;
+	}
+	public void setSupermercado(Supermercado supermercado) {
+		this.supermercado = supermercado;
+	}
+}
